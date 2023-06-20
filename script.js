ea89f802d1a31a8e908ca1b83a46ff7f3b08b67f
sudheer2144 <116650566+sudheer2144@users.noreply.github.com>
2023-06-20 16:14:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function remove(){
+
+  let elements=document.getElementById("colorSelect");
+
+  // console.log(elements.selectedIndex);
+  elements.remove(elements.selectedIndex);
+
+  // console.log(elements);
+}
